ce68e51b23be2a80c4a141f4adbc5ef9f3357078
tanmay-S12 <tanmaysankpal007@gmail.com>
2023-07-23 14:38:29 +0530

    commiting about changes

---
+++ myportfolio/src/Components/Skils.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './css/Skills.css'
+
+const Skils = () => {
+    return (
+        <>
+          {/* <p style={{ color: 'white'}}>Always learning and staying up-to-date with the latest trends and technologies in web development.</p> */}
+            <div className='main-card'>
+            <div className="card bg-body" style={{ width: "18rem" }}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Skils
